c2ea80f944656782f21ef806f991898b9b34ad5a
Aditya Kumar <69427863+Aditya-kumar121001@users.noreply.github.com>
2024-01-05 17:51:35 +0530

    Initial

---
+++ assets/js/script.js
@@ -0,0 +1,86 @@
+'use strict';
+
+/**
+ * element toggle function
+ */
+
+const elemToggleFunc = function (elem) { elem.classList.toggle("active"); }
+
+
+
+/**
+ * navbar toggle
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const overlay = document.querySelector("[data-overlay]");
+const navCloseBtn = document.querySelector("[data-nav-close-btn]");
+const navOpenBtn = document.querySelector("[data-nav-open-btn]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+
+const navElemArr = [overlay, navCloseBtn, navOpenBtn];
+
+/**
+ * close navbar when click on any navbar link
+ */
+
+for (let i = 0; i < navbarLinks.length; i++) { navElemArr.push(navbarLinks[i]); }
+
+/**
+ * addd event on all elements for toggling navbar
+ */
+
+for (let i = 0; i < navElemArr.length; i++) {
+  navElemArr[i].addEventListener("click", function () {
+    elemToggleFunc(navbar);
+    elemToggleFunc(overlay);
+  });
+}
+
+
+
+/**
+ * header active state
+ */
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function () {
+  window.scrollY >= 400 ? header.classList.add("active")
+    : header.classList.remove("active");
+}); 
+
+
+
+//Counter
+let valueDisplays = document.querySelectorAll(".num");
+let interval = 4000;
+
+valueDisplays.forEach((valueDisplay) => {
+  let startValue = 0;
+  let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+  let duration = Math.floor(interval / endValue);
+  let counter = setInterval(function () {
+    startValue +=1;
+    valueDisplay.textContent = startValue;
+    if (startValue == endValue) {
+      clearInterval(counter);
+    }
+  }, duration);
+});
+
+//Animation
+document.addEventListener("DOMContentLoaded", function () {
+  // Function to add 'animated' class to elements
+  function animateOnLoad() {
+    var heroContent = document.querySelector('.hero-content');
+    var heroBanner = document.querySelector('.hero-banner');
+
+    // Add 'animated' class to trigger the animation
+    heroContent.classList.add('animated');
+    heroBanner.classList.add('animated');
+  }
+
+  // Animate on page load
+  animateOnLoad();
+});
